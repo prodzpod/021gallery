36f4b3db192adf3dfd33b95f1a84b10bd75c51b5
unknown <prodzpod>
2022-07-03 23:11:35 +0900

    feat: new layout

--- js/main.js
+++ js/main.js
@@ -1,118 +1,119 @@
-let page = 0;
-let npage = 3;
-let selected = -1;
-let subpage = 1;
-let nsubpage = 2;
-let combo = 0;
+window.onload = handleLoad;
+window.onresize = handleResize;
+document.addEventListener("wheel", handleWheel);
+document.addEventListener("touchstart", handleTouchStart);
+document.addEventListener("touchmove", handleTouchMove);
+
+let LAYER = 0;
+let PAGES = [0];
+let PAGE = [0];
+
+function handleLoad() {
+    handleResize();
+    let main = insertElement('div', e('page-wrapper').firstElementChild, 'page-row');
+    for (let i = 0; i < CONTENT.length; i++) loadContent([i], CONTENT[i], main);
+    PAGES[0] = CONTENT.length;
+}
+
+function handleResize() {
+    document.documentElement.style.setProperty('--w', window.innerWidth + "px");
+    document.documentElement.style.setProperty('--h', window.innerHeight + "px");
+}
+
+function loadContent(page, content, el) {
+    let child = insertElement('div', el, 'page-cell');
+    insertElement('div', child, 'page-inner', content.content);
+    if (content.subpage?.length) {
+        let sub = insertElement('div', child, 'subpage');
+        sub.setAttribute('id', 'page-' + page.join('-'));
+        for (let i = 0; i < content.subpage.length; i++) loadContent([...page, i], content.subpage[i], sub);
+    }
+    child.addEventListener("click", enterPage);
+}
+
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}50
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) return;
+  const xDiff = xDown - evt.touches[0].clientX;
+  const yDiff = yDown - evt.touches[0].clientY;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) movePage(-1);
+    else movePage(1);
+  } else {
+    if (yDiff > 0) exitPage();
+    else enterPage();
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
 
 let busy = false;
-
-const deviceType = () => {
-    const ua = navigator.userAgent;
-    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) return "tablet";
-    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) return "mobile";
-    return "desktop";
-};
-
-let timeout;
-
-window.onload = function(event) {
-    let i = 0;
-    let inner = e('main').firstElementChild;
-    for (let el of CONTENT) {
-        let block = insertElement('div', inner, 'block');
-        block.setAttribute('id', 'main-' + i);
-        block.setAttribute('onclick', 'onClick(' + i + ')');
-        i++;
-        let content = insertElement('div', block, ['content', 'vlist']);
-        insertSpacer(content);
-        content.innerHTML += el.content;
-        insertSpacer(content);
-        let subpages = insertElement('div', block, 'subpages');
-        for (let page of el.subpages) {
-            let block = insertElement('div', subpages, 'block');
-            let content = insertElement('div', block, ['content', 'vlist']);
-            insertSpacer(content);
-            content.innerHTML += page;
-            insertSpacer(content);
-        }
-    }
-    npage = i;
-    nsubpage = 2;
-    onScrolled();
-    if (deviceType() === "mobile") {
-        e('main').onscroll = function() {
-            if (!busy) {
-                page = e('main').scrollLeft / document.documentElement.clientWidth;
-                e('page-bg').style.marginLeft = -document.documentElement.clientWidth * (page + subpage - 1);
-                clearTimeout(timeout);
-                timeout = setTimeout(() => { onScrolled() }, 500);
-            }
-        }
-        
-        e('sub').onscroll = function() {
-            if (!busy) {
-                subpage = e('sub').scrollLeft / document.documentElement.clientWidth;
-                subpage = clamp(0, subpage, nsubpage);
-                e('page-bg').style.marginLeft = -document.documentElement.clientWidth * (page + subpage - 1);
-                clearTimeout(timeout);
-                timeout = setTimeout(() => { onScrolled() }, 50);
-            }
-        }
-    }
-}
-
-window.onwheel = function(event) {
-    let amount = event.deltaY / 2000 + combo;
-    if (combo === 0 || Math.sign(combo) === Math.sign(amount)) {
-        combo += Math.sign(amount) / 0.01
-        if (Math.abs(combo) > 0.05) combo = clamp(-0.05, combo, 0.05);
-    } else combo = 0;
-    if (selected !== -1) {
-        subpage += amount;
-        if (combo === -0.05) subpage = 0.95;
-    }
-    else {
-        page += amount;
-    }
-    onScrolled();
-}
-
-function onScrolled() {
-    page = clamp(0, page, npage - 1);
-    subpage = clamp(0, subpage, nsubpage);
-    if (selected !== -1 && subpage < 1) {
-        selected = -1;
-        page += subpage - 1;
-        subpage = 1;
-        page = clamp(0, page, npage - 1);
-        subpage = clamp(0, subpage, nsubpage);
-        e('page-wrapper').style.marginTop = 0;
-        e('page-bg').style.opacity = 1;
-        busy = true;
-        setTimeout(() => { busy = false; }, 500);
-    }
-    let vpage = deviceType() === "mobile" ? page : (Math.trunc(page) + ((page % 1) * (page % 1)));
-    let vsubpage = deviceType() === "mobile" ? subpage : (Math.trunc(subpage) + ((subpage % 1) * (subpage % 1)));
-    e('main').scrollLeft = document.documentElement.clientWidth * vpage;
-    e('page-bg').style.marginLeft = -document.documentElement.clientWidth * (vpage + vsubpage - 1);
-    e('sub').scrollLeft = document.documentElement.clientWidth * vsubpage;
-}
-
-function onClick(page) {
-    selected = page;
-    loadSubpages(page);
-    subpage = 1;
-    onScrolled();
-    e('page-wrapper').style.marginTop = -document.documentElement.clientHeight;
-    e('page-bg').style.opacity = 0;
+function handleWheel(evt) {
+    if (busy) return;
+    console.log(evt.deltaY)
+    if (evt.deltaY < 0 && PAGE[LAYER] === 0) exitPage();
+    else movePage(Math.sign(evt.deltaY));
     busy = true;
-    setTimeout(() => { busy = false; }, 500);
+    setTimeout(() => { busy = false; }, 350);
 }
 
-function loadSubpages(page) {
-    cleanElement(e('sub').firstElementChild);
-    let target = e('main-' + page).getElementsByClassName('subpages')[0];
-    e('sub').firstElementChild.innerHTML = '<div class="block"></div>' + target.innerHTML;
-    nsubpage = target.getElementsByClassName('block').length;
+function movePage(dir) {
+    if (dir === 0 || 0 > (PAGE[LAYER] + dir) || (PAGE[LAYER] + dir) >= PAGES[LAYER]) return;
+    PAGE[LAYER] += dir;
+    updatePage();
+}
+
+function enterPage() {
+    if (!e('page-' + PAGE.join('-'))) return;
+    let main = insertElement('div', e('page-wrapper').firstElementChild, 'page-row');
+    LAYER++;
+    main.setAttribute('id', 'row-' + LAYER);
+    let subpage = e('page-' + PAGE.join('-'));
+    PAGES.push(subpage.childElementCount);
+    PAGE.push(0);
+    for (let i = 0; i < PAGE.reduce((prev, cur) => prev + cur, 0); i++) insertElement('div', main, 'page-empty');
+    for (let el of Array.from(subpage.childNodes)) main.appendChild(el);
+    cleanElement(subpage);
+    updatePage();
+}
+
+function exitPage() {
+    if (LAYER <= 0) return;
+    PAGES.pop();
+    PAGE.pop();
+    for (let el of Array.from(e('row-' + LAYER).getElementsByClassName('page-empty'))) el.parentElement.removeChild(el);
+    for (let el of Array.from(e('row-' + LAYER).childNodes)) e('page-' + PAGE.join('-')).appendChild(el);
+    e('row-' + LAYER).parentElement.removeChild(e('row-' + LAYER));
+    LAYER--;
+    updatePage();
+}
+
+let rX = 0, rY = 0;
+function updatePage() {
+    let int = setInterval(() => {
+        let x = PAGE.reduce((prev, cur) => prev + cur, 0);
+        let y = LAYER;
+        rX = lerp(rX, x, 0.5); rY = lerp(rY, y, 0.5);
+        e('page-wrapper').firstElementChild.style.marginLeft = "calc(" + (-rX) + " * var(--w))";
+        e('page-wrapper').firstElementChild.style.marginTop = "calc(" + (-rY) + " * var(--h))";
+        if (Math.hypot(rX - x, rY - y) < 0.00001) clearInterval(int);
+    }, 50);
 }
